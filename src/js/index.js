38f1905115ad306901cdbfcc12b18646aaa1e0c7
tiffanyona6 <tiffanyona6@gmail.com>
2024-05-20 21:49:40 +0200

    what

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
 import '../scss/styles.scss';
+
+//- Crea una función que reciba un número, la función debe imprimir una cuenta atrás desde ese número hasta 0.
+
+for (cnst number of numbers) {
+    console.log (number+100);
+}
